281471e24ca4f778c46c38f6bb8b6e238d246860
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-07-08 13:33:52 +0330

    refactore

--- main.go
+++ main.go
@@ -3,34 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
 
-func renderTemplate(res http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl + ".html")
-
-	err := parsedTemplate.Execute(res, nil)
-	if err != nil {
-		_, _ = fmt.Fprintf(res, "Error executing template")
-	}
-
-}
-
-func home(res http.ResponseWriter, req *http.Request) {
-	renderTemplate(res, "home.page.tmpl")
-
-}
-
-func about(res http.ResponseWriter, req *http.Request) {
-	renderTemplate(res, "about.page.tmpl")
-
-}
-
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/about", about)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	fmt.Println(fmt.Sprintf("Listening on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
