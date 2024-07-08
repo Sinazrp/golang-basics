d0c82be1ecd1beeb28cec644e2111b747c8233de
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-07-08 13:05:37 +0330

    learn multi handler

--- main.go
+++ main.go
@@ -5,16 +5,44 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func home(res http.ResponseWriter, req *http.Request) {
+	_, _ = fmt.Fprintf(res, "Welcome to the Home!")
+
+}
+func Divide(res http.ResponseWriter, req *http.Request) {
+	f, err := divideValues(100.0, 0)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, _ = fmt.Fprintf(res, "%d / %d = %f", 100, 10, f)
+}
+
+func about(res http.ResponseWriter, req *http.Request) {
+	sum, _ := addValues(2, 1)
+	_, _ = fmt.Fprintf(res, fmt.Sprintf("some of numbers is %d", sum))
+
+}
+func addValues(x, y int) (int, error) {
+
+	return x + y, nil
+}
+func divideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fprintf, err := fmt.Fprintf(writer, "Hello World")
-		if err != nil {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/divide", Divide)
 
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("number of bytes written: %d", fprintf))
-
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Listening on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
