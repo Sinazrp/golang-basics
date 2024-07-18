d80b0a443ab96f06f30bced5c099366cf5a229d7
Sinazrp <Sina.zrp@gmail.com>
2024-07-19 00:36:19 +0330

    use chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"golang-bookingwebapp/pkg/config"
 	"golang-bookingwebapp/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	//mux := pat.New()
+	//mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Get("/home", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 
 }
